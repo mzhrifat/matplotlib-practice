316bcf5a85c9ace9a2b61a5d6435693442c66c18
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-24 02:12:10 +0600

    linestyle commit

--- w3_mat1.py
+++ w3_mat1.py
@@ -7,10 +7,26 @@ ypoints = np.array([3, 8, 1, 10])
 
 plt.plot(ypoints, marker = '3')
 plt.show()
-"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 y=np.array([3,8,1,10])
-plt.plot(y,'o-.',marker='s',mec='r',mfc='r')
+plt.plot(y,marker='s',ms=20,mec='y',mfc='y',linestyle='-.',color='r',linewidth='20')
+plt.show()
+
+"""
+
+#multiple line
+import matplotlib.pyplot as plt
+import numpy as np
+
+x1=np.array([0,1,2,3])
+y1=np.array([3,8,1,10])
+x2=np.array([0,1,2,3])
+y2=np.array([6,2,7,11])
+
+plt.plot(x1, y1, marker='o', ms=10, mec='y', mfc='y', color='r', linewidth=2,linestyle='--')  # প্রথম লাইন
+plt.plot(x2, y2, marker='s', ms=10, mec='b', mfc='b', color='g', linewidth=2,linestyle='-.')  # দ্বিতীয় লাইন
+
 plt.show()
