70513ac362b5885d4bf58f8c0befb564f9e5b781
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-25 02:47:41 +0600

    sizes and alpha commit

--- w3_mat1.py
+++ w3_mat1.py
@@ -153,15 +153,31 @@ plt.show()
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
-y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+#x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+#y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
 #colors =np.array(["red","green","blue","yellow","pink","black","orange","purple","beige","brown","gray","cyan","magenta"])
 #comormap
-colors = np.array([0, 10, 20, 30, 40, 45, 50, 55, 60, 70, 80, 90, 100])
+#colors = np.array([0, 10, 20, 30, 40, 45, 50, 55, 60, 70, 80, 90, 100])
 #plt.scatter(x,y,c=colors)
 
-plt.scatter(x,y,c=colors, cmap='viridis')
+#plt.scatter(x,y,c=colors, cmap='viridis')
+#colorbar and comormap add
+#plt.colorbar()
+
+#size add
+#sizes= np.array([20,50,100,200,500,1000,60,90,10,300,600,800,75])
+#plt.scatter(x,y,s=sizes)
+
+#add alpha
+x=np.random.randint(100,size=(100))
+y=np.random.randint(100,size=(100))
+
+colors=np.random.randint(100,size=(100))
+sizes=10* np.random.randint(100,size=(100))
+
+plt.scatter(x,y,c=colors,s=sizes,alpha=0.5,cmap='nipy_spectral')
 plt.colorbar()
+
 plt.show()
 
 
