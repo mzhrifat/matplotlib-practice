261b74c75d81968507ffd3d4df3694d4c4e22f72
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-25 02:17:33 +0600

    scatter commit

--- w3_mat1.py
+++ w3_mat1.py
@@ -77,7 +77,7 @@ plt.grid(axis='y',linestyle='--',linewidth=0.5,color='r')
 plt.show()
 
 
-"""
+
 
 #subplot
 #column
@@ -109,6 +109,7 @@ import numpy as np
 #plot1
 x=np.array([0,1,2,3])
 y=np.array([3,8,1,10])
+
 plt.subplot(2,1,1)
 plt.plot(x,y)
 plt.title("Incomes")
@@ -119,6 +120,18 @@ y=np.array([10,12,14,15])
 plt.subplot(2,1,2)
 plt.plot(x,y)
 plt.title("Sales")
-
 plt.suptitle("Daily")
 plt.show()
+"""
+
+#SCATTER
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=np.array([5,7,8,7,2,17])
+y=np.array([99,86,87,88,111,86])
+
+plt.scatter(x,y)
+plt.show()
+
