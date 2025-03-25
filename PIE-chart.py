91cd24e0c27ddf75110b7565697d6502a2e367c9
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-26 01:20:03 +0600

    labels commit

--- PIE-chart.py
+++ PIE-chart.py
@@ -6,3 +6,12 @@ x=np.array([35,35,15,15])
 plt.pie(x)
 plt.show()
 """
+
+#labels
+import matplotlib.pyplot as plt
+import numpy as np
+x=np.array([35,25,10,30])
+
+mylabels=["Apples","Bananas","Cherries","Dates"]
+plt.pie(x,labels=mylabels)
+plt.show()
