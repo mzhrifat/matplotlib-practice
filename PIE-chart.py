6bc4404a36be61f9a95270593a6ca2ef4e9cbeb7
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-26 01:25:20 +0600

    explode commit

--- PIE-chart.py
+++ PIE-chart.py
@@ -13,5 +13,9 @@ import numpy as np
 x=np.array([35,25,10,30])
 
 mylabels=["Apples","Bananas","Cherries","Dates"]
-plt.pie(x,labels=mylabels)
+
+#also add angles(startangles)
+#ADD Explode
+myexplode=[0.2,0.2,0.2,0.2]
+plt.pie(x,labels=mylabels,startangle=90,explode=myexplode)
 plt.show()
