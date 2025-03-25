a497e6537019e84371bd9a0d5f24a386e1ace0de
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-26 01:32:05 +0600

    colors commit

--- PIE-chart.py
+++ PIE-chart.py
@@ -17,5 +17,12 @@ mylabels=["Apples","Bananas","Cherries","Dates"]
 #also add angles(startangles)
 #ADD Explode
 myexplode=[0.2,0.2,0.2,0.2]
-plt.pie(x,labels=mylabels,startangle=90,explode=myexplode)
+
+mycolors=["black","green","hotpink","b"]
+#add shadow
+#plt.pie(x,labels=mylabels,startangle=90,explode=myexplode,shadow=True)
+
+#add colors
+plt.pie(x,labels=mylabels,startangle=90,explode=myexplode,shadow=True,colors=mycolors)
+
 plt.show()
